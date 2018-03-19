1484de23c68c85f1b9de52120d32f366fface241
G-Ray <g.bonneville@qwant.com>
2018-03-19 16:24:26 +0100

    initial Settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-export default function Devices() {
+import Button from '../components/Button/Button';
+// import DeviceCard from '../components/DeviceCard';
+import TextInput from '../components/TextInput/TextInput';
+
+import './Settings.css';
+
+export default function Devices(props) {
+
+  const user = {
+    lastname: 'Bonneville',
+    firstname: 'Geoffrey',
+    pseudo: 'G-Ray'
+  };
+
   return (
-    <div>
-      <h2>Settings page</h2>
+    <div className="Settings" style={{ backgroundColor: '#f5f7fa', paddingTop: '1px' }}>
+
+      <h1 style={{marginLeft: '16px'}}>Your Profile</h1>
+
+      <div className="profile">
+        <div className="avatar">
+        </div>
+        <div className="fields">
+          <TextInput label='Last Name' defaultValue={user.lastname} />
+          <TextInput label='First Name' defaultValue={user.firstname} />
+          <TextInput label='Username (Displayed)' defaultValue={user.pseudo} />
+        </div>
+      </div>
+
+      <h1 style={{marginLeft: '16px'}}>Masq Parameters</h1>
+
+      <div className="button-container">
+        <Button label="SUPPRIMER MON COMPTE"/>
+      </div>
     </div>
   );
 }
