ccb19efeee5e2be1c279d5ba0aa02348d6cbaa75
G-Ray <g.bonneville@qwant.com>
2018-03-19 16:15:22 +0100

    TextInput: Accept defaultValue and fix styling

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -5,7 +5,7 @@ import './TextInput.css';
 export default function TextInput(props) {
   return (
     <div className="TextInput">
-      <input type="text"/>
+      <input type="text" defaultValue={props.defaultValue} />
       <label>{props.label}</label>
     </div>
   )
