38292db56153e2ed08e848c184a20b87d5bf758f
G-Ray <g.bonneville@qwant.com>
2018-03-20 11:48:08 +0100

    Notification: Colorize svgs and better positioning

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,28 +1,16 @@
 import React from 'react'
+import ReactSVG from 'react-svg'
 
 import './Notification.css'
 
-const style = {
-  width: '378px',
-  height: '40px',
-  borderRadius: '3px',
-  border: 'solid 1px #e5e5e5',
-  margin: 'auto',
-  verticalAlign: 'middle',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  alignItems: 'center'
-}
-
 export default function Notification (props) {
   return (
-    <div style={style}>
-      <div id='notif'>
-        <img style={{ height: '15px', marginLeft: '16px', 'marginRight': '16px' }} src='/assets/Infos.svg' alt='infos logo' />
+    <div className='Notification'>
+      <div className='leftNotif'>
+        <ReactSVG className='infos-icon' path='/assets/Infos.svg' />
         <p>{props.text}</p>
       </div>
-      <img style={{ height: '9px', marginRight: '16px' }} src='/assets/Close.svg' alt='close logo' />
+      <ReactSVG className='close-icon' path='/assets/Close.svg' />
     </div>
   )
 }
