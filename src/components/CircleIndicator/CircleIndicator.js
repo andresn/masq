25743bde7af376353d6a746324e97e793352e901
G-Ray <g.bonneville@qwant.com>
2018-03-21 14:26:35 +0100

    CircleIndicator: rm log

--- src/components/CircleIndicator/CircleIndicator.js
+++ src/components/CircleIndicator/CircleIndicator.js
@@ -3,7 +3,6 @@ import React from 'react'
 import './CircleIndicator.css'
 
 export default function CircleIndicator (props) {
-  console.log(props)
   return (
     <div className='circlestatus' style={{border: 'solid 2px' + props.color}} />
   )
