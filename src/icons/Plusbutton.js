0585721138abcd2ee34d3211aef8da08ff5cd76b
G-Ray <g.bonneville@qwant.com>
2018-03-21 16:55:34 +0100

    standardify Plusbutton.js

--- src/icons/Plusbutton.js
+++ src/icons/Plusbutton.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react'
 
 const Plusbutton = props => (
   <svg
-    xmlnsXlink="http://www.w3.org/1999/xlink"
+    xmlnsXlink='http://www.w3.org/1999/xlink'
     width={120}
     height={120}
-    viewBox="0 0 120 120"
+    viewBox='0 0 120 120'
     {...props}
   >
     <defs>
-      <circle id="a" cx={60} cy={60} r={60} />
+      <circle id='a' cx={60} cy={60} r={60} />
       <path
-        id="b"
-        d="M78.667 60c0 1.4-.934 2.333-2.334 2.333h-14v14c0 1.4-.933 2.334-2.333 2.334-1.4 0-2.333-.934-2.333-2.334v-14h-14c-1.4 0-2.334-.933-2.334-2.333 0-1.4.934-2.333 2.334-2.333h14v-14c0-1.4.933-2.334 2.333-2.334 1.4 0 2.333.934 2.333 2.334v14h14c1.4 0 2.334.933 2.334 2.333z"
+        id='b'
+        d='M78.667 60c0 1.4-.934 2.333-2.334 2.333h-14v14c0 1.4-.933 2.334-2.333 2.334-1.4 0-2.333-.934-2.333-2.334v-14h-14c-1.4 0-2.334-.933-2.334-2.333 0-1.4.934-2.333 2.334-2.333h14v-14c0-1.4.933-2.334 2.333-2.334 1.4 0 2.333.934 2.333 2.334v14h14c1.4 0 2.334.933 2.334 2.333z'
       />
     </defs>
-    <g fill="none" fillRule="evenodd">
-      <use fill="#FFF" xlinkHref="#a" />
-      <circle cx={60} cy={60} r={59} stroke="#CCC" strokeWidth={2} />
-      <use fill="#CCC" fillRule="nonzero" xlinkHref="#b" />
+    <g fill='none' fillRule='evenodd'>
+      <use fill='#FFF' xlinkHref='#a' />
+      <circle cx={60} cy={60} r={59} stroke='#CCC' strokeWidth={2} />
+      <use fill='#CCC' fillRule='nonzero' xlinkHref='#b' />
     </g>
   </svg>
-);
+)
 
-export default Plusbutton;
+export default Plusbutton
