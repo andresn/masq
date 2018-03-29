f0c3c3c65e14559f9338fe5d7761ecad0b5e2e80
G-Ray <g.bonneville@qwant.com>
2018-03-29 16:25:11 +0200

    Plusbutton: set Ids a and b

--- src/icons/Plusbutton.js
+++ src/icons/Plusbutton.js
@@ -9,16 +9,16 @@ const Plusbutton = props => (
     {...props}
   >
     <defs>
-      <circle id='a' cx={60} cy={60} r={60} />
+      <circle id='plus-button-svg-a' cx={60} cy={60} r={60} />
       <path
-        id='b'
+        id='plus-button-svg-b'
         d='M78.667 60c0 1.4-.934 2.333-2.334 2.333h-14v14c0 1.4-.933 2.334-2.333 2.334-1.4 0-2.333-.934-2.333-2.334v-14h-14c-1.4 0-2.334-.933-2.334-2.333 0-1.4.934-2.333 2.334-2.333h14v-14c0-1.4.933-2.334 2.333-2.334 1.4 0 2.333.934 2.333 2.334v14h14c1.4 0 2.334.933 2.334 2.333z'
       />
     </defs>
     <g fill='none' fillRule='evenodd'>
-      <use fill='#FFF' xlinkHref='#a' />
+      <use fill='#FFF' xlinkHref='#plus-button-svg-a' />
       <circle cx={60} cy={60} r={59} stroke='#CCC' strokeWidth={2} />
-      <use fill='#CCC' fillRule='nonzero' xlinkHref='#b' />
+      <use fill='#CCC' fillRule='nonzero' xlinkHref='#plus-button-svg-b' />
     </g>
   </svg>
 )
