063baafc582b4fdda018afc5b7a37ab905ccd7be
G-Ray <g.bonneville@qwant.com>
2018-03-30 15:38:00 +0200

    Card: Apply grayscale 50% on image when disabled

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,12 +8,17 @@ import './Card.css'
 export default function Card (props) {
   const { image, title, description, color, enabled, children } = props
 
+  const style = enabled
+    ? { backgroundImage: 'url(' + image + ')' }
+    : {
+      backgroundImage: 'url(' + image + ')',
+      filter: 'grayscale(50%)'
+    }
+
   return (
     <div className='Card'>
       {image
-        ? (<div className='image'
-          style={{backgroundImage: 'url(' + image + ')'}}
-        />)
+        ? (<div className='image' style={style} />)
         : null}
       <div className='flex'>
         <div className='content'>
