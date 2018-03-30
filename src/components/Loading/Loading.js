8cb358a972492d4f73168867c8965f1997f1c91c
G-Ray <g.bonneville@qwant.com>
2018-03-30 16:23:27 +0200

    Loading component (three animated dots)

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Loading.css'
+
+export default function Loading () {
+  return (
+    <div className='Loading'>
+      <div className='loading-dots' />
+      <div className='loading-dots' />
+      <div className='loading-dots' />
+    </div>
+  )
+}
