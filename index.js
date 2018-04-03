0e5a2e0cde8610bd23456b4f5f645935543df773
G-Ray <g.bonneville@qwant.com>
2018-04-03 12:32:43 +0200

    Initital electron-dev script

---
+++ index.js
@@ -0,0 +1,14 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({width: 800, height: 600})
+
+  // and load the index.html of the app.
+  win.loadURL('http://localhost:3000')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+}
+
+app.on('ready', createWindow)
