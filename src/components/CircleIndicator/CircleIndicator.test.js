7ba5f09e4350fbf488ffd95cfebf4be4a4a813bc
G-Ray <g.bonneville@qwant.com>
2018-04-04 16:47:51 +0200

    CircleIndicator snapshots

--- src/components/CircleIndicator/CircleIndicator.test.js
+++ src/components/CircleIndicator/CircleIndicator.test.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import CircleIndicator from './CircleIndicator'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<CircleIndicator color='blue' />, div)
+it('renders CircleIndicator with color blue', () => {
+  const component = renderer.create(<CircleIndicator color='blue' />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders CircleIndicator with color red', () => {
+  const component = renderer.create(<CircleIndicator color='red' />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
