ac6c2d5c6f07d9351d004359205a2806bdb7bf06
G-Ray <g.bonneville@qwant.com>
2018-04-04 16:33:08 +0200

    Avatar tests: Snapshots

--- src/components/Avatar/Avatar.test.js
+++ src/components/Avatar/Avatar.test.js
@@ -1,31 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import Avatar from './Avatar'
-// import ReactTestUtils from 'react-dom/test-utils'
 
-it('renders without crashing without upload', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Avatar />, div)
+it('renders Avatar without upload ability', () => {
+  const component = renderer.create(<Avatar />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
-it('renders without crashing with upload', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Avatar upload />, div)
+it('renders Avatar with upload ability', () => {
+  const component = renderer.create(<Avatar upload />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
 
-it('renders without crashing with upload and image', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Avatar upload image='https://randomuser.me/api/portraits/women/79.jpg' />, div)
+it('renders Avatar with upload ability and an image background', () => {
+  const component = renderer.create(<Avatar upload image='https://randomuser.me/api/portraits/women/79.jpg' />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
-
-// fit('fileHandler() should be called when clicking on the avatar to upload', () => {
-//   const spy = jest.fn()
-//   const div = document.createElement('div')
-//   const comp = ReactDOM.render(<Avatar upload fileHandler={spy} />, div)
-//   // const node = ReactDOM.findDOMNode(comp).getElementsByClassName('Avatar')[0]
-//   const node = ReactDOM.findDOMNode(comp).getElementsByClassName('inputfile')[0]
-//   ReactTestUtils.Simulate.change(node)
-//   // console.log(node)
-//   ReactTestUtils.Simulate.click(node)
-//   expect(spy.mock.calls.length).toBe(1)
-// })
