3a60c783aec878393edf8e12419dbc68ae1e01fb
G-Ray <g.bonneville@qwant.com>
2018-04-04 17:03:15 +0200

    Tabs snapshots

--- src/components/Tabs/Tabs.test.js
+++ src/components/Tabs/Tabs.test.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import Tabs from './Tabs'
 import { MemoryRouter } from 'react-router'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MemoryRouter><Tabs tabs={[]} /></MemoryRouter>, div)
+import { Smartphone, Apps } from 'icons'
+
+const tabs = [
+  { label: 'Devices', link: '/devices', icon: <Smartphone /> },
+  { label: 'Applications', link: '/applications', icon: <Apps /> }
+]
+
+it('renders Tabs with one tabs', () => {
+  const component = renderer.create(
+    <MemoryRouter><Tabs tabs={[tabs[0]]} /></MemoryRouter>
+  )
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Tabs with two tabs', () => {
+  const component = renderer.create(
+    <MemoryRouter><Tabs tabs={tabs} /></MemoryRouter>
+  )
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
