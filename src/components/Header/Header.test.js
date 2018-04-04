72d2eff3ceb45952ab70564b90204b4a9100733c
G-Ray <g.bonneville@qwant.com>
2018-04-04 16:51:47 +0200

    Header snapshots

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Header from './Header'
+import renderer from 'react-test-renderer'
 import { MemoryRouter } from 'react-router'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MemoryRouter><Header /></MemoryRouter>, div)
+import Header from './Header'
+
+it('renders Header', () => {
+  const component = renderer.create(<MemoryRouter><Header /></MemoryRouter>)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Header with shadow', () => {
+  const component = renderer.create(<MemoryRouter><Header shadow /></MemoryRouter>)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Header with a user', () => {
+  const user = {
+    firstname: 'some firstname',
+    lastname: 'some lastname',
+    image: 'https://randomuser.me/api/portraits/women/79.jpg'
+  }
+  const component = renderer.create(<MemoryRouter><Header user={user} /></MemoryRouter>)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
