f9dbb6631f38a49d6933dfa48b38e8ab81cc6b70
G-Ray <g.bonneville@qwant.com>
2018-04-04 16:44:47 +0200

    Card: Snapshots

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,8 +1,33 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Card from './Card'
+import renderer from 'react-test-renderer'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Card enabled color='blue' title='sometitle' />, div)
+it('renders Card disabled', () => {
+  const component = renderer.create(<Card enabled color='blue' title='sometitle' />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Card enabled', () => {
+  const component = renderer.create(<Card enabled={false} color='blue' title='sometitle' />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Card with a description', () => {
+  const component = renderer.create(
+    <Card enabled={false} color='blue' title='sometitle'
+      description='some description'
+    />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Card with an image', () => {
+  const component = renderer.create(
+    <Card enabled={false} color='blue' title='sometitle'
+      image='https://images.pexels.com/photos/63703/pexels-photo-63703.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'
+    />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
