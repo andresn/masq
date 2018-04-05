acfbc9f861b89f8ca71200c8720097e5fc2d0808
G-Ray <g.bonneville@qwant.com>
2018-04-05 14:24:34 +0200

    Update Loading snapshot with one user

--- src/pages/Loading/Loading.test.js
+++ src/pages/Loading/Loading.test.js
@@ -3,7 +3,13 @@ import renderer from 'react-test-renderer'
 import Loading from './Loading'
 
 it('renders Loading page', () => {
-  const component = renderer.create(<Loading user={{}} />)
+  const user = {
+    image: 'https://randomuser.me/api/portraits/women/79.jpg',
+    firstname: 'Alicia',
+    lastname: 'Ford',
+    username: 'Mustang'
+  }
+  const component = renderer.create(<Loading user={user} />)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
