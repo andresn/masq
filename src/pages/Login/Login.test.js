18b97f78d8500919aa199b6064e807f5d98a0111
G-Ray <g.bonneville@qwant.com>
2018-04-05 14:19:24 +0200

    Login snapshots

--- src/pages/Login/Login.test.js
+++ src/pages/Login/Login.test.js
@@ -1,9 +1,30 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import Login from './Login'
 import { MemoryRouter } from 'react-router'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<MemoryRouter><Login users={[]} auth={() => true} /></MemoryRouter>, div)
+it('renders Login without users', () => {
+  const component = renderer.create(<MemoryRouter><Login users={[]} auth={() => true} /></MemoryRouter>)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders Login with users', () => {
+  const users = [
+    {
+      image: 'https://randomuser.me/api/portraits/women/79.jpg',
+      firstname: 'Alicia',
+      lastname: 'Ford',
+      username: 'Mustang'
+    },
+    {
+      image: 'https://randomuser.me/api/portraits/men/1.jpg',
+      firstname: 'Jeffrey',
+      lastname: 'Hoffman',
+      username: 'Jeff'
+    }
+  ]
+  const component = renderer.create(<MemoryRouter><Login users={users} auth={() => true} /></MemoryRouter>)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
