35364acaeebc42ddd799128c584138e66e355909
G-Ray <g.bonneville@qwant.com>
2018-04-05 14:15:21 +0200

    Loading page snapshots

--- src/pages/Loading/Loading.test.js
+++ src/pages/Loading/Loading.test.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import Loading from './Loading'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Loading user={{}} />, div)
+it('renders Loading page', () => {
+  const component = renderer.create(<Loading user={{}} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
