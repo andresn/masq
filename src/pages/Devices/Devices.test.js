06471f1b318a64a419df9c674684cb7ebef5e36b
G-Ray <g.bonneville@qwant.com>
2018-04-05 14:13:51 +0200

    Devices page: snapshots

--- src/pages/Devices/Devices.test.js
+++ src/pages/Devices/Devices.test.js
@@ -1,8 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+
 import Devices from './Devices'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Devices devices={[]} />, div)
+it('renders no devices', () => {
+  const component = renderer.create(<Devices devices={[]} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders one disabled devices', () => {
+  const devices = [{ name: 'Some device', color: '#86e991', enabled: false }]
+  const component = renderer.create(<Devices devices={devices} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders one enabled devices', () => {
+  const devices = [{ name: 'Some device', color: '#86e991', enabled: true }]
+  const component = renderer.create(<Devices devices={devices} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders one new devices', () => {
+  const devices = [{ name: 'Some device', color: '#86e991', enabled: false, new: true }]
+  const component = renderer.create(<Devices devices={devices} />)
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
 })
