5922d530432e20a66f958e5a59614c75b9980dd8
G-Ray <g.bonneville@qwant.com>
2018-04-06 10:05:25 +0200

    lib/index: eslint-disable-next-line

--- src/lib/index.js
+++ src/lib/index.js
@@ -378,6 +378,7 @@ const clearStorage = () => {
 const generateUUID = () => {
   return 'xxxx-xxxx-xxxx-xxxx-xxxx'.replace(/[x]/g, (c) => {
     const r = Math.random() * 16 | 0
+    // eslint-disable-next-line
     const v = c === 'x' ? r : (r & 0x3 | 0x8)
     return v.toString(16)
   })
