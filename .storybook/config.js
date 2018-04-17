a4e4300ba7d918d40c9c897c2da69e2ae56c5bf8
G-Ray <g.bonneville@qwant.com>
2018-04-17 15:54:47 +0200

    Use storybook addon-info using decorator

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 import 'typeface-asap'
 import 'App.css'
@@ -8,5 +9,7 @@ const req = require.context('../src/components', true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
 }
+
+addDecorator((story, context) => withInfo('common info')(story)(context));
 
 configure(loadStories, module);
