5ceeb829c32f010ece0da2412d2ee92d9ccdc200
G-Ray <g.bonneville@qwant.com>
2018-04-19 10:08:03 +0200

    Update LoadingDots with new colors

--- src/components/LoadingDots/LoadingDots.js
+++ src/components/LoadingDots/LoadingDots.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import './LoadingDots.css'
 
+const colors = ['#4ebbc2', '#3394d2', '#e53b5b']
+
 export default function LoadingDots () {
   return (
     <div className='Loading'>
-      <div className='loading-dots' />
-      <div className='loading-dots' />
-      <div className='loading-dots' />
+      <div style={{backgroundColor: colors[0]}} className='loading-dots' />
+      <div style={{backgroundColor: colors[1]}} className='loading-dots' />
+      <div style={{backgroundColor: colors[2]}} className='loading-dots' />
     </div>
   )
 }
