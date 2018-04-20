a8ae4278547d8c865baf999b71c757f4983e93a1
G-Ray <g.bonneville@qwant.com>
2018-04-20 16:03:05 +0200

    Modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Close } from 'icons'
+
+const styles = {
+  modal: {
+    backgroundColor: 'white',
+    width: 511,
+    height: 670,
+    boxShadow: '0 2px 16px 0 rgba(0, 0, 0, 0.16)'
+  },
+  close: {
+    float: 'right',
+    marginTop: 20,
+    marginRight: 20,
+    color: '#191919',
+    opacity: 0.4
+  }
+}
+
+export default function Modal (props) {
+  return (
+    <div style={styles.modal}>
+      <Close style={styles.close} width={9} height={9} onClick={props.onClose} />
+      {props.children}
+    </div>
+  )
+}
