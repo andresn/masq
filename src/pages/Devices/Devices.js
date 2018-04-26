c50c1d22ad3bf54b187c7cd85cf7dd011dc934e7
G-Ray <g.bonneville@qwant.com>
2018-04-26 10:43:11 +0200

    Devices page: Add NewDevice modal

--- src/pages/Devices/Devices.js
+++ src/pages/Devices/Devices.js
@@ -4,40 +4,60 @@ import PropTypes from 'prop-types'
 import { Button } from 'components'
 
 import DeviceCard from './DeviceCard/DeviceCard'
+import { AddDevice } from 'modals'
 
 import './Devices.css'
 
-export default function Devices (props) {
-  const { devices } = props
+export default class Devices extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isModalOpened: false
+    }
+    this.toggleModal = this.toggleModal.bind(this)
+  }
 
-  return (
-    <div className='Devices'>
-      <div>
-        <div className='title-container'>
-          <h1>Currently active</h1>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
-        </div>
-        <div className='cards-grid'>
-          {devices.map((device, index) =>
-            <DeviceCard key={index} index={index} device={device} />
-          )}
-        </div>
+  toggleModal (state) {
+    this.setState({
+      isModalOpened: state
+    })
+  }
 
-        <div className='title-container'>
-          <h1>My devices</h1>
-          <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+  render () {
+    const { devices } = this.props
+
+    return (
+      <div className='Devices'>
+        <div>
+          {this.state.isModalOpened &&
+            <AddDevice onClose={() => this.toggleModal(false)} />
+          }
+          <div className='title-container'>
+            <h1>Currently active</h1>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+          </div>
+          <div className='cards-grid'>
+            {devices.map((device, index) =>
+              <DeviceCard key={index} index={index} device={device} />
+            )}
+          </div>
+
+          <div className='title-container'>
+            <h1>My devices</h1>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+          </div>
+          <div className='cards-grid'>
+            {devices.map((device, index) =>
+              <DeviceCard key={index} index={index} device={device} />
+            )}
+          </div>
         </div>
-        <div className='cards-grid'>
-          {devices.map((device, index) =>
-            <DeviceCard key={index} index={index} device={device} />
-          )}
+        <div className='sidebar'>
+          <Button secondary label='ADD A NEW DEVICE' onClick={() => this.toggleModal(true)} />
         </div>
       </div>
-      <div className='sidebar'>
-        <Button secondary label='ADD A NEW DEVICE' />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Devices.propTypes = {
