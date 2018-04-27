9b95ebfeba08288124e9a6dca766e89cd782fcb3
G-Ray <g.bonneville@qwant.com>
2018-04-27 17:44:07 +0200

    Tweak style of AuthApp Modal

--- src/modals/AuthApp/AuthApp.js
+++ src/modals/AuthApp/AuthApp.js
@@ -4,14 +4,67 @@ import { Modal, Button } from 'components'
 
 import './AuthApp.css'
 
+const styles = {
+  header: {
+    // backgroundColor: '#252a39',
+    height: 150,
+    width: '100%'
+  },
+  title: {
+    fontSize: '16px',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: '0.1px',
+    textAlign: 'center'
+  },
+  appTitle: {
+    fontSize: '32px',
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: 'normal',
+    letterSpacing: '0.2px',
+    textAlign: 'center'
+  },
+  description: {
+    width: 380,
+    paddingBottom: 32,
+    fontSize: '14px',
+    fontWeight: '500',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    lineHeight: 1.57,
+    letterSpacing: 'normal',
+    textAlign: 'center',
+    color: 'var(--blue-200)'
+  },
+  buttons: {
+    display: 'grid',
+    gridTemplateColumns: 'auto auto',
+    gridGap: 32
+  }
+}
+
 export default class AuthApp extends React.Component {
   render () {
     return (
       <Modal onClose={this.props.onClose}>
+        <div style={styles.header} />
         <div className='AuthApp'>
-          <h1>New App request</h1>
-          <p style={{paddingBottom: 64}}>The app {this.props.app.url} is requesting access to your masq</p>
-          <div>
+          <p style={styles.title}>New connection request from:</p>
+          <p style={styles.appTitle}>{this.props.app.url}</p>
+          <p style={styles.description}>
+            This notification appears because that application asks permission
+            to your Masq storage.
+          </p>
+          <p style={styles.description}>
+            If you are not at the origin of this request or if you have question,
+            please contact our help center.
+          </p>
+
+          <div style={styles.buttons}>
             <Button label='Reject' onClick={this.props.onReject} />
             <Button label='Accept' onClick={this.props.onAccept} />
           </div>
