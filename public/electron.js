3ef09b3156eec3eefe22b12a5abb112c5971761c
G-Ray <g.bonneville@qwant.com>
2018-05-02 16:05:21 +0200

    electron.js: Set min size

--- public/electron.js
+++ public/electron.js
@@ -5,7 +5,12 @@ const isDev = process.env.ENV === 'development'
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({width: 800, height: 600})
+  const win = new BrowserWindow({
+    minWidth: 1024,
+    minHeight: 768,
+    width: 1024,
+    height: 768
+  })
 
   console.log(path.join(__dirname, '../build/index.html'))
 
