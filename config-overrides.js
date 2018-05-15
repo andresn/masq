052ab36e9720dd00f2d9e7c3fc1036334f4dad68
G-Ray <g.bonneville@qwant.com>
2018-05-15 16:15:18 +0200

    webpack config: avoid warning about bindings

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,8 @@
 module.exports = function override (config, env) {
   // do stuff with the webpack config...
   config.target = 'electron-renderer'
+  config.externals = {
+    bindings: 'require("bindings")'
+  }
   return config
 }
