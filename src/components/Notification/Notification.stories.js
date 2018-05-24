e682f9a8f891d9568ccc1928901df7732aea7e6f
G-Ray <g.bonneville@qwant.com>
2018-05-24 14:34:56 +0200

    Remove Notification component

--- src/components/Notification/Notification.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import Notification from './Notification'
-
-storiesOf('Notification', module)
-  .add('notification', () => (
-    <Notification text='some notification' />
-  ))
