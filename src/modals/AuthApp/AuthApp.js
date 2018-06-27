425eafc4b15eb171aa1918cb716d7575a2f40315
G-Ray <g.bonneville@qwant.com>
2018-06-27 10:39:42 +0200

    AuthApp: Move styles to css

--- src/modals/AuthApp/AuthApp.js
+++ src/modals/AuthApp/AuthApp.js
@@ -6,63 +6,25 @@ import { Modal, Button } from 'components'
 
 import './AuthApp.css'
 
-const styles = {
-  title: {
-    fontSize: '16px',
-    fontWeight: 'normal',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: 'normal',
-    letterSpacing: '0.1px',
-    textAlign: 'center'
-  },
-  appTitle: {
-    fontSize: '32px',
-    fontWeight: 'bold',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: 'normal',
-    letterSpacing: '0.2px',
-    textAlign: 'center'
-  },
-  description: {
-    width: 380,
-    paddingBottom: 32,
-    fontSize: '14px',
-    fontWeight: '500',
-    fontStyle: 'normal',
-    fontStretch: 'normal',
-    lineHeight: 1.57,
-    letterSpacing: 'normal',
-    textAlign: 'center',
-    color: 'var(--blue-200)'
-  },
-  buttons: {
-    display: 'grid',
-    gridTemplateColumns: 'auto auto',
-    gridGap: 32
-  }
-}
-
 export default class AuthApp extends React.Component {
   render () {
     const { onClose, app, securityCode = 1234, onAccept, onReject } = this.props
     return (
       <Modal onClose={onClose} width={511}>
         <div className='AuthApp'>
-          <p style={styles.title}><Trans>New connection request from</Trans></p>
-          <p style={styles.appTitle}>{app.url}</p>
+          <p className='title'><Trans>New connection request from</Trans></p>
+          <p className='appTitle'>{app.url}</p>
           <input className='securityCode' placeholder={securityCode} readOnly />
-          <p style={styles.description}>
+          <p className='description'>
             <Trans>This notification appears because that application asks permission
             to use your Masq storage.</Trans>
           </p>
-          <p style={styles.description}>
+          <p className='description'>
             <Trans>If you are not at the origin of this request or if you have question,
             please contact our help center.</Trans>
           </p>
 
-          <div style={styles.buttons}>
+          <div className='buttons'>
             <Button label={i18next.t('Reject')} onClick={onReject} />
             <Button label={i18next.t('Accept')} onClick={onAccept} />
           </div>
