f02028f9342ff625db1f85fad170cbb10d9234c7
G-Ray <g.bonneville@qwant.com>
2018-08-01 15:23:52 +0200

    Add tray / dock (need a mac to test) integration. Fix #47

--- public/electron.js
+++ public/electron.js
@@ -1,20 +1,62 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Tray, Menu } = require('electron')
 const { autoUpdater } = require('electron-updater')
 const path = require('path')
 
+const os = require('os')
+
+// 'Linux' on Linux, 'Darwin' on macOS and 'Windows_NT'
+console.log('os', os.type())
+
 const isDev = process.env.ENV === 'development'
+
+let win = null
+
+const contextMenu = Menu.buildFromTemplate([
+  { label: 'exit', type: 'normal' }
+])
+
+function toggleWindow () {
+  if (win.isVisible()) {
+    win.hide()
+  } else {
+    win.show()
+  }
+}
+
+// on Linux and Windows_NT
+function createTray () {
+  const tray = new Tray(path.join(__dirname, './icon.png'))
+  tray.on('right-click', toggleWindow)
+  tray.on('double-click', toggleWindow)
+  tray.on('click', toggleWindow)
+  tray.setToolTip('This is my application.')
+  tray.setContextMenu(contextMenu)
+}
+
+// On macOS
+function createDock () {
+  app.dock.setMenu(contextMenu)
+}
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     minWidth: 600,
     minHeight: 768,
     width: 1024,
     height: 768
+    // titleBarStyle: 'hidden'
   })
 
   // and load the index.html of the app.
   win.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`)
+
+  const osType = os.type()
+  if (osType === 'macOS') {
+    createDock()
+  } else { // Linux of Windows
+    createTray()
+  }
 
   // Open the DevTools.
   if (isDev) {
@@ -28,5 +70,5 @@ function createWindow () {
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-  app.quit()
+  win.hide()
 })
